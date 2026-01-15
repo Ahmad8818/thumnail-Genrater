a52792b1c97e780cf9ee03810d1932853ddc7d01
Ahmad8818 <ahmad8818510@gmail.com>
2026-01-15 14:53:10 +0500

    cookie updated

--- server/server.ts
+++ server/server.ts
@@ -25,13 +25,20 @@ app.use(cors({
     origin:[
         'http://localhost:5173',
         'http://localhost:3000',
+        //add frontend url
     ],credentials:true
 }))
 app.use(session({
     secret:process.env.SESSION_SECRET as string,
     resave:false,
     saveUninitialized:false,
-    cookie:{maxAge:1000 * 60 * 60 * 24 * 7}, // 7Days
+    cookie:{
+        maxAge:1000 * 60 * 60 * 24 * 7,
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        sameSite:'none',
+        path:'/'
+    }, 
     store:MongoStore.create({
         mongoUrl:process.env.MONGODB_URI as string,
         collectionName:'sessions'
